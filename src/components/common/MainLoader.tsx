f98562ec05ef2e3a61a3a13a3198fe197c3f6c36
Abdul Fatir Ansari <abdulfatiransari2@gmail.com>
2025-07-03 16:00:56 +0500

    fix: default wallet and loading size

--- src/components/common/MainLoader.tsx
+++ src/components/common/MainLoader.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function MainLoader({ text }: { text?: string }) {
   return (
     <div className="flex flex-col justify-center items-center p-0">
-      <Image alt="" src={"/loading.gif"} width={400} height={400} />
+      <Image alt="" src={"/loading.gif"} width={100} height={100} />
       <span className="text-[#1A4FBA]">{text}</span>
     </div>
   );
--- src/context/WalletContext.tsx
+++ src/context/WalletContext.tsx
@@ -25,11 +25,16 @@ createAppKit({
     socials: false,
     email: false,
   },
+  enableWalletConnect: false,
+  featuredWalletIds: [
+    "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393", //phantom
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", //metamask
+  ],
   includeWalletIds: [
-    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
-    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
-    "c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a",
-    "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393",
+    "a797aa35c0fadbfc1a53e7f675162ed5226968b44a19ee3d24385c64d1d3c393", //phantom
+    "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96", //metamask
+    "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", // trust
+    // "c03dfee351b6fcc421b4494ea33b9d4b92a984f87aa76d1663bb28705e95034a",
   ],
 });
 
